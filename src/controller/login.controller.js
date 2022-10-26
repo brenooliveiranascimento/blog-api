2c5437efb36d4a7d6c9b4214e9d192ca8bdb680b
Breno Nascimento <satsukebare23@gmail.com>
2022-10-26 20:40:17 -0300

    remove body validation in controller

--- src/controller/login.controller.js
+++ src/controller/login.controller.js
@@ -3,9 +3,6 @@ const { createToken } = require('../utils/jwt.utils');
 
 const createUser = async (req, res) => {
   const { email, password } = req.body;
-  // if (!email || !password) {
-  //   return res.status(400).json({ message: 'Some required fields are missing' });
-  // }
   const { error } = await loginServices.registerUser({ email, password });
   if (error) {
     return res.status(400).json({ message: error.message });
